fcadc56bcf49a155c7bf4b1d4190d99376841813
Quinchy <deguzmancyriljames@gmail.com>
2025-02-04 09:27:04 +0800

    .

--- apps/web/pages/virtual-try-on.js
+++ apps/web/pages/virtual-try-on.js
@@ -422,24 +422,26 @@ export default function VirtualTryOnPage() {
 
 const styles = {
   container: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    position: "fixed", // Make sure the container is fixed to the viewport
+    top: 0,
+    left: 0,
     width: "100vw",
     height: "100vh",
     backgroundColor: "#000",
     overflow: "hidden",
-    margin: "0",
-    padding: "0",
+    margin: 0,
+    padding: 0,
   },
   cameraContainer: {
     position: "relative",
+    width: "100%",
+    height: "100%",
   },
   // Video element displays the camera feed normally.
   video: {
     width: "100%",
     height: "100%",
-    objectFit: "contain",
+    objectFit: "cover",
     backgroundColor: "#000",
   },
   // Canvas is absolutely positioned over the video and remains transparent.
