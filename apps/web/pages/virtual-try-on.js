57ce1c6ef5aae125514ce0fe361cb3623b10f014
Quinchy <deguzmancyriljames@gmail.com>
2025-03-30 02:24:52 +0800

    .

--- apps/web/pages/virtual-try-on.js
+++ apps/web/pages/virtual-try-on.js
@@ -201,7 +201,7 @@ export default function VirtualTryOnPage() {
       }
       overlayWidth = overlayWidth * 1.5;
       const overlayX = shoulderCenterX - overlayWidth / 2;
-      const overlayY = shoulderCenterY - overlayHeight * 0.12;
+      const overlayY = shoulderCenterY - overlayHeight * 0.05;
       return { overlayX, overlayY, overlayWidth, overlayHeight };
     } else if (clothingType === "OUTERWEAR") {
       // OUTERWEAR parameters
