5240576accddbce1b21be9bec32498ea4eb99d14
Quinchy <deguzmancyriljames@gmail.com>
2025-04-24 17:53:19 +0800

    .

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,7 +3,34 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["geist"],
   images: {
-    domains: ['abvikodzzsvmdljgcdhq.supabase.co', 'jbqushznasoaufpwsjfx.supabase.co', 'bjymhfrftpseknpnlbse.supabase.co','cdn.fashn.ai'],
+    domains: [
+      "abvikodzzsvmdljgcdhq.supabase.co",
+      "jbqushznasoaufpwsjfx.supabase.co",
+      "bjymhfrftpseknpnlbse.supabase.co",
+      "cdn.fashn.ai",
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // Match all API routes
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:8100",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
   },
 };
 
